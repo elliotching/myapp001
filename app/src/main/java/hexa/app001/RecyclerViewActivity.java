16fee97274ffbbec16e8c20636084cd53b6f5e1b
chinglee.tang <chinglee.tang@hexainitiative.com>
2019-07-23 11:46:27 +0800

    included recyclerview

--- app/src/main/java/hexa/app001/RecyclerViewActivity.java
+++ app/src/main/java/hexa/app001/RecyclerViewActivity.java
@@ -1,8 +1,12 @@
 package hexa.app001;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+
+import java.util.ArrayList;
 
 public class RecyclerViewActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class RecyclerViewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_view);
+
+        // ...
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvContacts = (RecyclerView) findViewById(R.id.rvContacts);
+
+        // Initialize contacts
+        ArrayList<Contact> contacts = Contact.createContactsList(20);
+        // Create adapter passing in the sample user data
+        ContactsAdapter adapter = new ContactsAdapter(contacts);
+        // Attach the adapter to the recyclerview to populate items
+        rvContacts.setAdapter(adapter);
+
+        // Set layout manager to position the items
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
+        // That's all!
     }
 }
