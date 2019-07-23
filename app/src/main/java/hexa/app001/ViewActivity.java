7d30db28c1452d3e49f201c37d8c3e56c1eccc40
chinglee.tang <chinglee.tang@hexainitiative.com>
2019-07-23 18:10:19 +0800

    view activity

--- app/src/main/java/hexa/app001/ViewActivity.java
+++ app/src/main/java/hexa/app001/ViewActivity.java
@@ -1,14 +1,26 @@
 package hexa.app001;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ViewActivity extends AppCompatActivity {
-
+    Activity activity = this;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+        this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            activity.finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
