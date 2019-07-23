1a47190d8806bb836a726b129bb14407d7e78c73
chinglee.tang <chinglee.tang@hexainitiative.com>
2019-07-23 15:19:14 +0800

    added background effect and added on touch event listener.

--- app/src/main/java/hexa/app001/ContactsAdapter.java
+++ app/src/main/java/hexa/app001/ContactsAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -35,7 +36,7 @@ public class ContactsAdapter extends
         View contactView = inflater.inflate(R.layout.item_contact, parent, false);
 
         // Return a new holder instance
-        ViewHolder viewHolder = new ViewHolder(contactView);
+        ViewHolder viewHolder = new ViewHolder(context, contactView);
         return viewHolder;
     }
 
@@ -59,21 +60,36 @@ public class ContactsAdapter extends
 
     // Provide a direct reference to each of the views within a data item
     // Used to cache the views within the item layout for fast access
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener  {
         // Your holder should contain a member variable
         // for any view that will be set as you render a row
         public TextView nameTextView;
         public Button messageButton;
+        private Context context;
 
         // We also create a constructor that accepts the entire item row
         // and does the view lookups to find each subview
-        public ViewHolder(View itemView) {
+        public ViewHolder(Context context, View itemView) {
             // Stores the itemView in a public final member variable that can be used
             // to access the context from any ViewHolder instance.
             super(itemView);
 
             nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
             messageButton = (Button) itemView.findViewById(R.id.message_button);
+            // Store the context
+            this.context = context;
+            // Attach a click listener to the entire row view
+            itemView.setOnClickListener(this);
+        }
+        // Handles the row being being clicked
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition(); // gets item position
+            if (position != RecyclerView.NO_POSITION) { // Check if an item was deleted, but the user clicked it before the UI removed it
+                Contact contact = mContacts.get(position);
+                // We can access the data within the views
+                Toast.makeText(context, nameTextView.getText(), Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
--- app/src/main/java/hexa/app001/RecyclerViewActivity.java
+++ app/src/main/java/hexa/app001/RecyclerViewActivity.java
@@ -1,10 +1,13 @@
 package hexa.app001;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -25,9 +28,9 @@ public class RecyclerViewActivity extends AppCompatActivity {
         ContactsAdapter adapter = new ContactsAdapter(contacts);
         // Attach the adapter to the recyclerview to populate items
         rvContacts.setAdapter(adapter);
-
         // Set layout manager to position the items
         rvContacts.setLayoutManager(new LinearLayoutManager(this));
         // That's all!
+
     }
 }
