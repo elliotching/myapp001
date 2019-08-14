d2e27cbb0d354892a6ea61cee6ce7edf25fb005e
chinglee.tang <chinglee.tang@hexainitiative.com>
2019-08-14 14:56:21 +0800

    [] (chinglee.tang) remove unwanted method

--- app/src/main/java/hexa/app001/RecyclerViewMvpView.java
+++ app/src/main/java/hexa/app001/RecyclerViewMvpView.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 public interface RecyclerViewMvpView {
   
-  void onRetrievedSuccess();
-  
-  
   void populateRecyclerView(List<Movie> movies);
   
   void showError(String error);
